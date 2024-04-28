4e33d7a9ec1ebd1cc6f5953d2bd290a1d1daabd1
Adam James Rowe <adamjamesrowe@outlook.es>
2024-04-28 17:44:56 +0200

    changes

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const serverless = require('serverless-http')
 const bodyParser = require('body-parser');
-const router = require('./functions/tasks');
 
 require('dotenv').config();
 
 const app = express();
+const router = express.Router();
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -19,16 +19,8 @@ mongoose
   .catch(err => console.log(err));
 
 // Routes
-//app.use('/api/auth', require('./routes/auth'));
-//app.use('/api/tasks', require('./routes/tasks'));
-app.use('/functions/auth', router);
+app.use('/.netlify/functions/auth', router);
 app.use('/.netlify/functions/tasks', router);
 
 
 module.exports.handler = serverless(app);
-
-
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
