89ddac2b0d7a24f37e901e4774f9dc45d0331ba7
Adam James Rowe <adamjamesrowe@outlook.es>
2024-05-01 18:06:17 +0200

    again

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ mongoose
   .catch(err => console.log(err));
 
 // Routes
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
  app.use('/api/auth', require('./functions/auth'));
  app.use('/api/tasks', require('./functions/tasks'));
- 
+  
  app.get('/', (req, res) => {
   res.send('{"Hey this is my API running 🥳"}')
 });
