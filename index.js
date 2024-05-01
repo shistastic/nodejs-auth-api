3cc1c23e7e40b7dfd0a68c16090575997ceb4728
Adam James Rowe <adamjamesrowe@outlook.es>
2024-05-01 20:04:13 +0200

    Porfavor funciona ya

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const app = express();
+/** 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-const app = express();
+
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -15,15 +17,15 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
+**/
 // Routes
 const PORT = process.env.PORT || 4000;
 
  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+/** 
  app.use('/api/auth', require('./functions/auth'));
  app.use('/api/tasks', require('./functions/tasks'));
-  
+  */
  app.get('/', (req, res) => {
   res.send('{"Hey this is my API running 🥳"}')
 });
