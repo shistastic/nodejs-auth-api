a8626945a87b786445bf951162c25d9662dde47a
Adam James Rowe <adamjamesrowe@outlook.es>
2024-05-03 17:08:42 +0200

    All

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-/** 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const auth = require('./api/auth');
+const tasks = require('./api/tasks');
 
 require('dotenv').config();
-
-
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -17,17 +16,19 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-**/
+
 // Routes
 const PORT = process.env.PORT || 4000;
 
- app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-/** 
- app.use('/api/auth', require('./functions/auth'));
- app.use('/api/tasks', require('./functions/tasks'));
-  */
+ 
+ app.use('/api/auth', auth);
+ app.use('/api/tasks', tasks);
+
  app.get('/', (req, res) => {
   res.send('{"Hey this is my API running 🥳"}')
 });
+
+ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
  module.exports = app;
